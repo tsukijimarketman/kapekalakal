93f753e67ee060daa77e3571eebf0682109a2b74
Tsukijimarketman <gerickmvelasquez@gmail.com>
2025-08-26 11:02:09 +0800

    removed interceptor

--- src/config/api.tsx
+++ src/config/api.tsx
@@ -5,20 +5,6 @@ const api = axios.create({
   withCredentials: true,
 });
 
-//request interceptor to include auth token
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
-
 export default api;
 
 // API configuration
