6c89a69443e8fc362d11e37bc8866f44c4c1e96e
Tsukijimarketman <gerickmvelasquez@gmail.com>
2025-08-26 10:24:57 +0800

    build errors

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,23 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(), 
+    tailwindcss(),
+  ],
+  build: {
+    outDir: 'build',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom', 'react-router-dom'],
+          vendor: ['axios', 'jwt-decode'],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Increase chunk size warning limit to 1000KB
+  },
+  server: {
+    port: 3000,
+  },
 });
